cf4bf098454859b38c8412414444df42e6428f73
Josh Goldberg <diogenesthecynic@hotmail.com>
2014-12-03 06:33:08 -0500

    Documented MenuAgent
    Whoo.

--- js/MenuAgent.js
+++ js/MenuAgent.js
@@ -1,9 +1,19 @@
 /**
+ * Creates an instance of MenuAgent. The agent manipulates already-existing 
+ * HTML objects in the #menus element and modifies the body's .className to be
+ * .mobile, .tablet, or .desktop depending on device type and screen 
+ * resolution.
  * 
+ * @constructor
+ * @this {MenuAgent}
  */
 function MenuAgent() {
     this.sourceDir = location.href.slice(0, location.href.lastIndexOf("/"));
     this.parent = document.body;
+    
+    // http://stackoverflow.com/questions/11381673/detecting-a-mobile-browser
+    this.agent = navigator.userAgent || navigator.vendor || window.opera;
+    this.isMobile = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(this.agent) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(this.agent.substr(0, 4));
     
     this.checkMobile();
     this.getElements();
@@ -15,27 +25,32 @@ function MenuAgent() {
 }
 
 /**
+ * Hides the #menus container by giving it a .hidden className.
  * 
+ * @this {MenuAgent}
  */
 MenuAgent.prototype.hide = function () {
     this.container.className += " hidden";
 };
 
 /**
+ * Shows the #menus container by removing the .hidden className.
  * 
+ * @this {MenuAgent}
  */
 MenuAgent.prototype.show = function () {
     this.container.className = this.container.className.replace(" hidden", "");
 };
 
 /**
+ * Callback for when the page loads or the screen resizes. A set of rules for
+ * determining optimal display is followed, generally giving larger / more
+ * horizontal screens desktop, and smaller / more vertical screens mobile. The
+ * outcome is reflected in the parent .className (which is normally the body).
  * 
+ * @this {MenuAgent}
  */
 MenuAgent.prototype.checkMobile = function () {
-    // http://stackoverflow.com/questions/11381673/detecting-a-mobile-browser
-    var agent = navigator.userAgent || navigator.vendor || window.opera,
-        isMobile = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(agent) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(agent.substr(0, 4));
-
     // Extremely skinny (<=350px or 1x2) windows are always mobile
     if(innerWidth <= 350 || innerHeight > innerWidth * 2) {
         this.parent.className = "mobile";
@@ -49,7 +64,7 @@ MenuAgent.prototype.checkMobile = function () {
     }
     
     // Mobile detected is either tablet or mobile, depending on orientation
-    if(isMobile) {
+    if(this.isMobile) {
         if(innerWidth > innerHeight) {
             this.parent.className = "tablet";
         } else {
@@ -69,7 +84,10 @@ MenuAgent.prototype.checkMobile = function () {
 };
 
 /** 
+ * Stores the required HTML elements as member variables. Also calls
+ * this.getButtonsKeyed so they can be referenced by String id.
  * 
+ * @this {MenuAgent}
  */
 MenuAgent.prototype.getElements = function () {
     this.container = this.parent.querySelector("#menus");
@@ -86,7 +104,11 @@ MenuAgent.prototype.getElements = function () {
 };
 
 /**
+ * Converts the NodeList of button elements to an Object keying them by their
+ * element ID.
  * 
+ * @this {MenuAgent}
+ * @return {Object}
  */
 MenuAgent.prototype.getButtonsKeyed = function () {
     var output = {},
@@ -100,9 +122,20 @@ MenuAgent.prototype.getButtonsKeyed = function () {
 };
 
 /**
+ * Sets the interactive events for each button element. This involves having
+ * their help content displayed and having their background images set.
  * 
+ * @this {MenuAgent}
  */
 MenuAgent.prototype.setButtonEffects = function () {
+        /**
+         * Generic callback for each button's onMouseOver. The button's help
+         * content is displayed instead of the normal help blurb, such that
+         * the first sentence is the title and the rest the body.
+         * 
+         * @this {MenuAgent}
+         * @param {MouseEvent} event
+         */
     var buttonMouseOver = (function (event) {
             var button = event.target,
                 information = button.getAttribute("alt").split(/\.(.+)?/);
@@ -110,6 +143,16 @@ MenuAgent.prototype.setButtonEffects = function () {
             this.helpHead.innerHTML = information[0];
             this.helpParagraph.innerHTML = information[1];
         }).bind(this),
+        /**
+         * Generic callback for each button's onMouseOut. The help section's
+         * original content is displayed instead of the button's. 
+         * 
+         * @this {MenuAgent}
+         * @param {MouseEvent} event
+         * @remarks   This should be bound to setTimeout with a delay, so if 
+         *            the user moves the mouse between non-touching icons there
+         *            isn't a flicker between buttons.
+         */
         buttonMouseOut = (function (event) {
             var button = event.target,
                 information = button.getAttribute("alt").split(/\.(.+)?/);
@@ -140,7 +183,15 @@ MenuAgent.prototype.setButtonEffects = function () {
 };
 
 /**
+ * Switches the menus between classes, and binds their head element's to a call
+ * to the classes in reverse order. 
  * 
+ * @this {MenuAgent}
+ * @param {String} classOld
+ * @param {String} classNew
+ * @param {MouseEvent/TouchEvent} event
+ * @remarks   Typically the classes will be "menu-contracted" and 
+ *            "menu-expanded" in either order.
  */
 MenuAgent.prototype.toggleMenuClasses = function (classOld, classNew, event) {
     var head, menu, i;
@@ -161,25 +212,13 @@ MenuAgent.prototype.toggleMenuClasses = function (classOld, classNew, event) {
 };
 
 /**
+ * Sets the callbacks for "action"-style buttons (such as saving), where 
+ * clicking a button has no affect on any other buttons. Callbacks are 
+ * afterwords triggered by button onClicks.
  * 
- */
-MenuAgent.prototype.setButtonStatusCallbacks = function (callbacks) {
-    var button, i;
-    
-    for(i in callbacks) {
-        if(!callbacks.hasOwnProperty(i)) {
-            continue;
-        }
-        
-        button = this.buttonsKeyed[i];
-        button.onclick = this.setButtonActive.bind(
-            this, button, callbacks[i]
-        );
-    }
-};
-
-/**
- * 
+ * @this {MenuAgent}
+ * @param {Object} callbacks   An Object keying String button IDs to their
+ *                             Function callbacks.
  */
 MenuAgent.prototype.setButtonActionCallbacks = function (callbacks) {
     var button, i;
@@ -197,7 +236,37 @@ MenuAgent.prototype.setButtonActionCallbacks = function (callbacks) {
 };
 
 /**
+ * Sets the callbacks for "status"-style buttons (such as brush size), where
+ * enabling one disables all its siblings. Callbacks are afterwords triggered
+ * by button onClicks.
  * 
+ * @this {MenuAgent}
+ * @param {Object} callbacks   An Object keying String button IDs to their
+ *                             Function callbacks.
+ */
+MenuAgent.prototype.setButtonStatusCallbacks = function (callbacks) {
+    var button, i;
+    
+    for(i in callbacks) {
+        if(!callbacks.hasOwnProperty(i)) {
+            continue;
+        }
+        
+        button = this.buttonsKeyed[i];
+        button.onclick = this.setButtonActive.bind(
+            this, button, callbacks[i]
+        );
+    }
+};
+
+/**
+ * Utility Function to visually deactivate a button's siblings and give it an
+ * active .className. The callback Function is then called.
+ * 
+ * @this {MenuAgent}
+ * @param {HTMLElement} button
+ * @param {Function} callback
+ * @param {MouseEvent/TouchEvent}} event
  */
 MenuAgent.prototype.setButtonActive = function (button, callback, event) {
     var parent = button.parentElement,
@@ -212,7 +281,13 @@ MenuAgent.prototype.setButtonActive = function (button, callback, event) {
 };
 
 /**
+ * Utility Function to simply call a button's action. If an event is provided,
+ * it has preventDefault() and stopPropagation() called.
  * 
+ * @this {MenuAgent}
+ * @param {HTMLElement} button
+ * @param {Function} callback
+ * @param {MouseEvent/TouchEvent}} event
  */
 MenuAgent.prototype.callButtonAction = function (button, callback, event) {
     callback(event);
