4dbea7b0d7fe0f08137ee98992c8283f35fa27e1
Josh Goldberg <diogenesthecynic@hotmail.com>
2014-12-03 03:23:39 -0500

    Fix for screenshot only having background

--- js/RainbowScratchPaper.js
+++ js/RainbowScratchPaper.js
@@ -60,8 +60,8 @@ RainbowScratchPaper.prototype.saveScreenshot = function () {
     canvas.width = this.ScratchOffAgent.canvas.width;
     canvas.height = this.ScratchOffAgent.canvas.height;
     
-    context.drawImage(this.ScratchOffAgent.canvas, 0, 0);
     context.drawImage(this.BackgroundAgent.canvas, 0, 0);
+    context.drawImage(this.ScratchOffAgent.canvas, 0, 0);
     
     link.download = "RainbowScratchPaper Screenshot.png";
     link.href = canvas.toDataURL(format).replace(format, "image/octet-stream");
